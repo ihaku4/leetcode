e1b1e5dac579e7f6ad91050084c7f4ce3ad0357e
ihaku4 <ihaku4@gmail.com>
2015-02-03 20:40:28 +0800

    moore voting algorithm

--- majority-element.py
+++ majority-element.py
@@ -2,21 +2,23 @@ class Solution:
     # @param num, a list of integers
     # @return an integer
     def majorityElement(self, num):
-        if len(num) <= 2:
-            return num[0]
-        map = {}
-        throttle = len(num) / 2 + len(num) % 2
-        for n in num:
-            if n in map:
-                map[n] += 1
-                if map[n] == throttle:
-                    return n
+        vote = num[0]
+        count = 1
+        size = len(num)
+        for i in range(1, size):
+            if count == 0:
+                vote = num[i]
+                count += 1
+            elif vote == num[i]:
+                count += 1
             else:
-                map[n] = 1
-        return -1
+                count -= 1
+        return vote
 
 s = Solution()
 print s.majorityElement([1, 2, 3, 1, 1])
 print s.majorityElement([1, 2, 3, 2, 1, 3])
 print s.majorityElement([1])
 print s.majorityElement([2, 2])
+num = [1, 2, 1, 2, 1]
+print sorted(num)[len(num)/2]
