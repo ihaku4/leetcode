ea8dbb0acf312a70ded80f1cfd2abb4123a83970
ihaku4 <ihaku4@gmail.com>
2015-02-06 06:44:47 +0800

    string-to-integer-atoi remove float parse

--- string-to-integer-atoi.py
+++ string-to-integer-atoi.py
@@ -1,16 +1,32 @@
-import functools
+# import functools
 
 
 class Solution:
+    INT_MAX = 0x7fffffff
+    INT_MIN = -INT_MAX - 1
+    INVALID = 0
     map = {}
     for i in '0123456789':
         map[i] = ord(i) - ord('0')
     for i in 'ABCDEF':
         map[i] = ord(i) - ord('A') + 10
 
-    def mark_parse(func):
-        @functools.wraps(func)
+    def overflow_check(func):
+        # @functools.wraps(func)
         def wrapper(self, str, base=10):
+            num = func(self, str, base)
+            if num > Solution.INT_MAX:
+                return Solution.INT_MAX
+            elif num < Solution.INT_MIN:
+                return Solution.INT_MIN
+            return num
+        return wrapper
+
+    def mark_parse(func):
+        # @functools.wraps(func)
+        def wrapper(self, str, base=10):
+            if len(str) == 0:
+                return Solution.INVALID
             positive = True
             if str[0] == '-':
                 positive = False
@@ -22,6 +38,33 @@ class Solution:
             else:
                 num = func(self, str)
             return num if positive else -num
+        return wrapper
+
+    def not_empty(func):
+        def wrapper(self, str, base=10):
+            str = str.strip()
+            if len(str) == 0:
+                return Solution.INVALID
+            return func(self, str, base)
+        return wrapper
+
+    def validate(func):
+        def wrapper(self, str, base=10):
+            not_valid = {
+                2: lambda c: c not in '01',
+                8: lambda c: c not in '01234567',
+                # 10: lambda c: c not in '.0123456789',
+                10: lambda c: c not in '0123456789',
+                16: lambda c: c not in '0123456789ABCDEF',
+                }
+            for i in range(len(str)):
+                if not_valid[base](str[i]):
+                    if i == 0:
+                        return Solution.INVALID
+                    else:
+                        str = str[0:i]
+                        break
+            return func(self, str, base)
         return wrapper
 
     # integer literal types:
@@ -41,17 +84,20 @@ class Solution:
     # 3. trim head 0s, tail 0s
     # @return an integer
 
+    @not_empty
+    @overflow_check
     @mark_parse
     def atoi(self, str, base=10):
-        # validate TODO
-
         num = None
         str = str.upper()
-        if str.count('E') > 0 and not str.startswith('0X'):
-            num = self.parse_scientific_notation(str)
-        elif str.count('.') > 0:
-            num = self.parse_dot_float(str)
-        elif str.startswith('0X'):
+        # if str.count('E') > 0 and not str.startswith('0X'):
+        #     pass
+        #     num = self.parse_scientific_notation(str)
+        # elif str.count('.') > 0:
+        #     pass
+        #     num = self.parse_dot_float(str)
+        # else:
+        if str.startswith('0X'):
             num = self.parse_integer(str[2:], base=16)
         elif str.startswith('0B'):
             num = self.parse_integer(str[2:], base=2)
@@ -70,14 +116,19 @@ class Solution:
         e = self.parse_integer(tail)
         size = 1.0
         if e < 0:
-            for i in range(e, 0):
+            i = e
+            while i < 0:
                 size /= 10
+                i += 1
         else:
-            for i in range(e):
+            i = 0
+            while i < e:
                 size *= 10
+                i += 1
         return num * size
 
     @mark_parse
+    @validate
     def parse_integer(self, str, base=10):
         str = str.lstrip('0')
         if len(str) == 0:
@@ -85,19 +136,27 @@ class Solution:
         num = 0
         size = 1
         for i in str[::-1]:
+            if i not in Solution.map:
+                break
             num += Solution.map[i] * size
             size *= base
         return num
 
     @mark_parse
+    @validate
     def parse_dot_float(self, str, base=10):
-        head, tail = str.split('.')
+        sp = str.split('.', 1)
+        head = sp[0]
         num = self.parse_integer(head)
-        tail = tail.rstrip('0')
+        if len(sp) == 1:
+            return num
+        tail = sp[1].rstrip('0')
         if len(tail) == 0:
             return num + 0.0
         size = base * 1.0
         for i in tail:
+            if i not in Solution.map:
+                break
             num += Solution.map[i] / size
             size *= base
         return num
@@ -109,7 +168,7 @@ class Test(unittest.TestCase):
     def setUp(self):
         self.s = Solution()
 
-    def test_dot_float_test(self):
+    def _test_dot_float_test(self):
         # XXX how to test float equal?
         self.assertEqual(12.34, 12.34)
         self.assertEqual(1*10 + 2 + 3.0/10 + 4.0/100, 12.34)
@@ -126,7 +185,7 @@ class Test(unittest.TestCase):
         self.assertEqual(+1.23 * 1e+12, +1.23e+12)
         self.assertEqual(+1.23 * 1e-12, +1.23e-12)
 
-    def test_dot_float(self):
+    def _test_dot_float(self):
         self.assertEqual(self.s.atoi('12.34'), 12.34)
         self.assertEqual(self.s.atoi('012.34'), 12.34)
         self.assertEqual(self.s.atoi('012.3400'), 12.34)
@@ -142,12 +201,14 @@ class Test(unittest.TestCase):
         self.assertEqual(self.s.atoi('+012.3400'), +12.34000)
 
     # TODO how to test float?
-    def test_science_notation_float(self):
+    def _test_science_notation_float(self):
         # self.assertEqual(self.s.atoi('1.23e12'), 1.23e12)
         # self.assertEqual(self.s.atoi('1.23e+12'), 1.23e+12)
         # print self.s.atoi('1.23e-12'), 1.23e-12
         # print self.s.atoi('1.23e-12') == 1.23e-12
         # self.assertEqual(self.s.atoi('1.23e-12'), 1.23e-12)
+        print abs(self.s.atoi('1.23e12') - 1.23e12)
+        print self.s.atoi('1.23e12')
         self.assertTrue(abs(self.s.atoi('1.23e12') - 1.23e12) < 1e-8)
         self.assertTrue(abs(self.s.atoi('1.23e+12') - 1.23e+12) < 1e-8)
         self.assertTrue(abs(self.s.atoi('1.23e-12') - 1.23e-12) < 1e-8)
@@ -155,6 +216,8 @@ class Test(unittest.TestCase):
         self.assertTrue(abs(self.s.atoi('-1.23e12') - -1.23e12) < 1e-8)
         self.assertTrue(abs(self.s.atoi('-1.23e+12') - -1.23e+12) < 1e-8)
         self.assertTrue(abs(self.s.atoi('-1.23e-12') - -1.23e-12) < 1e-8)
+        self.assertTrue(
+            abs(self.s.atoi('+11e530408314') - +11e530408314) < 1e-8)
         print self.s.atoi('12', base=10)
         print self.s.atoi('12.0', base=16)
         print self.s.parse_integer('-F', base=16)
@@ -198,5 +261,19 @@ class Test(unittest.TestCase):
         self.assertEqual(self.s.atoi('-11101'), -11101)
         self.assertEqual(self.s.atoi('-111010000'), -111010000)
         self.assertEqual(self.s.atoi('-111009995'), -111009995)
+
+    def test_special_case(self):
+        self.assertEqual(self.s.atoi(''), Solution.INVALID)
+        self.assertEqual(self.s.atoi('   '), Solution.INVALID)
+        self.assertEqual(self.s.atoi(' 123  '), 123)
+        self.assertEqual(self.s.atoi('qwer 123  '), Solution.INVALID)
+        self.assertEqual(self.s.atoi('  123k23fsd  '), 123)
+        # self.assertEqual(self.s.atoi('  12.3k23fsd  '), 12.3)
+        self.assertEqual(self.s.atoi('  111k2.3k23fsd  '), 111)
+        self.assertEqual(self.s.atoi('0x7fffffff'), 0x7fffffff)
+        self.assertEqual(self.s.atoi('-0x80000000'), -0x80000000)
+        self.assertEqual(self.s.atoi('0x80000000'), 0x7fffffff)
+        self.assertEqual(self.s.atoi('-0x80000001'), -0x80000000)
+
 
 unittest.main()
