7ec716df65a14c9c1b0f6be32e2a89511236dc6c
ihaku4 <ihaku4@gmail.com>
2015-02-11 22:53:37 +0800

    climbing_stairs.py fibo solution

--- climbing_stairs.py
+++ climbing_stairs.py
@@ -2,6 +2,28 @@ class Solution:
     # @param n, an integer
     # @return an integer
     def climbStairs(self, n):
+        if n < 0:
+            return 0
+        if n < 2:
+            return 1
+        n1, n2 = 1, 1
+        i = 2
+        while i <= n:
+            n1Bak = n1
+            n1 = n2
+            n2 += n1Bak
+            i += 1
+        return n2
+
+    def fib(self, n):
+        if n < 0:
+            return 0
+        num, next = 1, 1
+        for i in range(n):
+            num, next = next, num + next
+        return num
+
+    def _climbStairs(self, n):
         solved = [0] * (n + 1)
         solved[0] = 1
         return self.climbStairsRec(n, solved)
@@ -32,6 +54,7 @@ class Test(unittest.TestCase):
         print self.s.climbStairs(2)
         for i in range(100):
             print i, self.s.climbStairs(i)
+            print i, self.s.fib(i)
 
 if __name__ == '__main__':
     unittest.main()
