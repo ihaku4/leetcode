d4de7820eb622955555eb0136a8bff9510910e72
ihaku4 <ihaku4@gmail.com>
2015-02-13 13:17:10 +0800

    clone_graph: queue and map. AC

---
+++ clone_graph.py
@@ -0,0 +1,44 @@
+# Definition for a undirected graph node
+class UndirectedGraphNode:
+    def __init__(self, x):
+        self.label = x
+        self.neighbors = []
+
+class Solution:
+    # @param node, a undirected graph node
+    # @return a undirected graph node
+    def cloneGraph(self, node):
+        if not node:
+            return None
+        queue = [node]
+        oldMap = {node.label: node}
+        newMap = {}
+        visit = 0
+        while visit < len(queue):
+            cur = queue[visit]
+
+            # create new and clone label
+            newNode = UndirectedGraphNode(cur.label)
+            # temperately link new neighbors to old
+            newNode.neighbors = cur.neighbors
+            newMap[newNode.label] = newNode
+
+            # iterate neighbors and check if not in map
+            for n in cur.neighbors:
+                if n.label not in oldMap:
+                    # push new 'old node' in neighbors to queue
+                    queue.append(n)
+                    # add neighbors to map
+                    oldMap[n.label] = n
+
+            visit += 1
+
+        # connect new node's neighbors to true neighbors
+        for label in newMap:
+            newNode = newMap[label]
+            newNeighbors = []
+            for neighbor in newNode.neighbors:
+                newNeighbors.append(newMap[neighbor.label])
+            newNode.neighbors = newNeighbors
+
+        return newMap[node.label]
