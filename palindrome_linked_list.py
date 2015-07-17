e9bb2f90070483421ed9c1451c9789b013addb88
ihaku4 <ihaku4@gmail.com>
2015-07-17 14:37:37 +0800

    refactor palindrome linked list

--- palindrome_linked_list.py
+++ palindrome_linked_list.py
@@ -30,36 +30,19 @@ class Solution:
         # reverse head half
         half_length = length / 2
 
-        if half_length >= 2:
-            # O ->  O ->        O ->
+        head, nxt  = None, head
+        while half_length >= 1:
+            head_next = nxt
+            nxt = nxt.next
+            # <- O  O ->        O ->
             # head  head_next   nxt
-            head_next = head.next
-            nxt = head.next.next
 
-            head.next = None
             head_next.next = head
-            # <- O  <- O        O ->
-            # head  head_next   nxt
-
             head = head_next
             # <- O  <- O        O ->
             #       head_next   nxt
             #       head
-
             half_length -= 1
-            
-            while half_length >= 2:
-                head_next = nxt
-                nxt = nxt.next
-                # <- O  O ->        O ->
-                # head  head_next   nxt
-
-                head_next.next = head
-                head = head_next
-                # <- O  <- O        O ->
-                #       head_next   nxt
-                #       head
-                half_length -= 1
 
         # compare head and tail
         head_middle_2 = nxt
@@ -75,14 +58,14 @@ class Solution:
         is_palindrome = head is None
 
         # restore head
-        while head_middle_1 is not None:
-            nxt_1 = head_middle_1.next
-            # <- O      <- O            O ->
-            # nxt_1     head_middle_1   head_middle_2
-            head_middle_1.next = head_middle_2
-            head_middle_2 = head_middle_1
-            head_middle_1 = nxt_1
-            # <- O          O ->            O ->
-            # head_middle_1 head_middle_2
+        #while head_middle_1 is not None:
+        #    nxt_1 = head_middle_1.next
+        #    # <- O      <- O            O ->
+        #    # nxt_1     head_middle_1   head_middle_2
+        #    head_middle_1.next = head_middle_2
+        #    head_middle_2 = head_middle_1
+        #    head_middle_1 = nxt_1
+        #    # <- O          O ->            O ->
+        #    # head_middle_1 head_middle_2
 
         return is_palindrome
