bc97fc06b14d5c59171ded04fa2810d0d525c4f9
ihaku4 <ihaku4@gmail.com>
2015-07-30 23:49:26 +0800

    remove old inefficient method

--- two_sum.py
+++ two_sum.py
@@ -2,23 +2,6 @@ class Solution:
     # @param {integer[]} nums
     # @param {integer} target
     # @return {integer[]}
-    def twoSum2(self, nums, target):
-        numsWithIndex = []
-        for i, n in enumerate(nums):
-            numsWithIndex.append((i + 1, n))
-        numsWithIndex.sort(cmp=lambda x, y: x[1] - y[1])
-
-        head, tail = 0, len(numsWithIndex) - 1
-        while head < tail:
-            if numsWithIndex[head][1] + numsWithIndex[tail][1] > target:
-                tail -= 1
-            elif numsWithIndex[head][1] + numsWithIndex[tail][1] < target:
-                head += 1
-            else:
-                return (min(numsWithIndex[head][0], numsWithIndex[tail][0]),
-                        max(numsWithIndex[head][0], numsWithIndex[tail][0]))
-        return None
-
     def twoSum(self, nums, target):
         numMap = {}
         for i, n in enumerate(nums):
